c3f692e984f22ca2160d11bd7a7481d910fa6768
DevHasin <125283705+Hasin125@users.noreply.github.com>
2024-11-21 13:44:01 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Age_Calculator_T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Age Calculator \n \n");
+
+            Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nIn what year were you born");
+            string yob = Console.ReadLine();
+            int yearB = Convert.ToInt32(yob);
+            int yearT = DateTime.Now.Year;
+
+          int age  = yearT - yearB;
+
+            Console.WriteLine($"\nDear {name} you are or will be of {age} years old this year. \n");
+            Console.WriteLine("Press Any Key To Exit...");
+
+            Console.ReadLine();
+        }
+    }
+}
